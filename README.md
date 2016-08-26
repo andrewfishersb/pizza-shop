af6d3b7663e180900f04744859ef11a36b6c2cb6
Andrew Fisher <andrewfishersb@gmail.com>
2016-08-26 13:53:31 -0700

    will now print out information about your pizza

--- README.md
+++ README.md
@@ -17,14 +17,14 @@ _1. It can ask the user for a one topping on their pizza._
 _2. It can ask the user for multiple toppings on their pizza._
 * _Input Example: Pepperoni, Olives, Onion, Pineapple_
 * _Output Example: [Pepperoni, Olives, Onion, Pineapple]_
-<!--  prices are subject to change -->
+
 _3. It can calculate the cost of one topping._
 * _Input Example: Pepperoni_
-* _Output Example: .50$_
+* _Output Example: $1.50_
 
 _4. It can calculate the cost of multiple toppings._
 * _Input Example: Pepperoni, Olives, Onion, Pineapple_
-* _Output Example: 1.25$_
+* _Output Example: $4.50_
 
 _5. It can ask for a pizza size._
 * _Input Example: large_
@@ -36,15 +36,15 @@ _6. It can calculate the cost of the specific pizza size._
 
 _7. It can combine the price of multiple toppings and pizza size_
 * _Input Example: large, Pepperoni, Olives, Onion, Pineapple_
-* _Output Example: $13.25_
-<!-- May or May not get to this tasks -->
+* _Output Example: $16.50_
+
 _8. It can ask the user for multiple pizzas_
 * _Input Example: [Large, Pepperoni, Olives], [Small, Onion, Pineapple]_
 * _Output Example: [[Large, Pepperoni, Olives], [Small, Onion, Pineapple]]_
 
 _9. It can calculate the price of multiple pizzas_
 * _Input Example: [Large, Pepperoni, Olives], [Small, Onion, Pineapple]_
-* _Output Example: $12.75 and $8.50_
+* _Output Example: $15.00 and $10.50_
 
 _10. It the user can check a delivery option and it will then ask for an address_
 * _Input Example: 555 Main Street_
--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,8 @@ function Pizza(size,toppings){ // contructors may change
   //possibly toppings for premium and regular
 }
 
-  function Topping(name){
-    this.name = name;
+  function Topping(type){
+    this.type = type;
     this.isPremium = false;
   }
 // Pizza.prototype.createOrder = function(pizza){
@@ -24,22 +24,24 @@ function Pizza(size,toppings){ // contructors may change
 //at first all toppings cost the same
 //if delivery add a charge
 Pizza.prototype.calculatePrice = function(){
-  // debugger;
   var numberOfToppings = this.toppings.length;
-  if(this.size==='small'){
+  var premToppings =0;
+  for(var i =0;i<numberOfToppings;i++){
+      if(this.toppings[i].isPremium){
+        premToppings++;
+      }
+  }
+  if(this.size==='Small'){
     this.price += 7.99 + numberOfToppings*1.25;
-
-  }else if(this.size==='medium'){
+  }else if(this.size==='Medium'){
     this.price += 9.99 + numberOfToppings*1.50;
-  }else if(this.size ==='large'){
+  }else if(this.size ==='Large'){
     this.price += 11.99 + numberOfToppings*1.75;
   }else{
     this.price+=13.99 + numberOfToppings*2.00;
   }
+  this.price += premToppings*.6;
   this.price = Math.round(this.price*100)/100;
-  if(Topping.isPremium){
-    this.price +=.6
-  }
   return this.price;
 }
 
@@ -48,44 +50,42 @@ Pizza.prototype.calculatePrice = function(){
 //Front-End
 
 $(function(){
+
   $("form").submit(function(event){
     event.preventDefault();
-    var inputSize = $("#size").val();
+    var inputName = $("user-name").val();
+    var inputSize = $("#select-size").val();
     //play around to see if i can make it more me
     var inputToppings = $("input:checkbox[name=toppings]:checked").map(function(){
+
       //takes the value from <input> being stripped and uses the name with a # to convert to the label which I named using the # + value
       var convertToLabel =$("#" + this.value).text();
-      console.log("")
-      //WORK HERE WHEN I GET BACK CHECK IF PREMIUM AND THEN TURN TOPPING INTO PREMIUM
-
-
-
-
-
-
-
-
-
-
-
-      return new Topping(convertToLabel);
+      var currentTopping =new Topping(convertToLabel);
+      //check if premium topping
+      if(this.value ==='sliced' ||this.value ==='steak' ||this.value ==='chicken' ||this.value ==='anchovies' ||this.value ==='salami'){
+        currentTopping.isPremium = true;
+      }
+      return currentTopping;
     }).get();
-
-
-
-    // valueToLabel(inputToppings);
-    console.log(inputToppings);
-
     var myPizza = new Pizza(inputSize,inputToppings);
-    console.log(myPizza);
     var price = myPizza.calculatePrice();
-    console.log(price);
+    $("#name").text(inputName);
+    $("#size").text(inputSize);
+    myPizza.toppings.forEach(function(topping){
+        $("#toppings").append("<li>"+topping.type+"</li>");
+    });
 
+
+
+    $("#charge").text(price);
+    //Adds a delivery fee
+    if($("#delivery-check").is(':checked')){
+        price += 2;
+        $("#deliver").text("Yes");
+    }else{
+      $("#deliver").text("No");
+    }
 
   });
-
-
-
-
 
 });
