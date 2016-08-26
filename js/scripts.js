ab8ebb10b06b6889590bd4aa9824a6ea26a71941
Andrew Fisher <andrewfishersb@gmail.com>
2016-08-26 11:04:04 -0700

    have a basic interface that put the value from the input tag into an array

--- js/scripts.js
+++ js/scripts.js
@@ -2,62 +2,88 @@
 function Person(){ // may not need unless multiple orders
   this.name = name;
   this.pizza = new Pizza(); // this may change
+  this.delivery = false;
 }
 
 function Pizza(size,toppings){ // contructors may change
   this.size = size;
-  this.toppings = [];
+  this.toppings = toppings;
   this.price = 0;
+  this.isPremium = false;
   //possibly toppings for premium and regular
 }
 
-Pizza.prototype.createOrder = function(pizza){
-  this.toppings.push(this.size);
-}
+// Pizza.prototype.createOrder = function(pizza){
+//   this.toppings.push(this.size);
+// }
+
+
 //at first all toppings cost the same
-Pizza.prototype.calculatePrice = function(toppings){
-  var numberOfToppings = toppings.length-1;
-  if(size==='small'){
+//if delivery add a charge
+Pizza.prototype.calculatePrice = function(){
+  // debugger;
+  var numberOfToppings = this.toppings.length;
+  if(this.size==='small'){
     this.price += 7.99 + numberOfToppings*1.25;
 
-  }else if(size==='medium'){
+  }else if(this.size==='medium'){
     this.price += 9.99 + numberOfToppings*1.50;
-  }else if(size ==='large'){
+  }else if(this.size ==='large'){
     this.price += 11.99 + numberOfToppings*1.75;
   }else{
     this.price+=13.99 + numberOfToppings*2.00;
   }
-  this.price = Math.round(price*100)/100;
-  return price;
+  this.price = Math.round(this.price*100)/100;
+  if(this.isPremium){
+    this.price +=.6
+  }
+  return this.price;
 }
-
-//reg = all vegs,pepperoni, bacon, italian sausage(non sliced), beef,ham
-//1.25 reg - small
-//1.50 reg - medium
-//1.75 reg - large
-//2.00 reg - x-large
- //non reg sliced, steak, chicken, salami, anchovies
-//1.85 non-reg - small
-//2.10 non-reg - medium
-//2.35 non-reg - large
-//2.60 non-reg - x-large
-
 
 
 
 //Front-End
 
 $(function(){
-  $("form").submit(function(){
+  //function that takes the values and conver them to the label
+  // var valueToLabel = function(array){
+  //   array.forEach(function(item){
+  //     console.log("got here: " + this.value);
+  //
+  //     item = $("#"+this.value).text();
+  //   })
+  // }
+
+
+
+  $("form").submit(function(event){
+    event.preventDefault();
+    var inputSize = $("#size").val();
+    //play around to see if i can make it more me
+    var inputToppings = $("#toppings input:checkbox:checked").map(function(){
+
+      console.log("got here: " + this.value)
+        return "got here: " + this.value;
+    }).get();
+
+    // var inputToppings = $("#toppings input:checkbox:checked").map(function(){
+    //     return this.value;
+    // }).get();
+
+
+    // valueToLabel(inputToppings);
+    console.log(inputToppings);
+
+    var myPizza = new Pizza(inputSize,inputToppings);
+    console.log(myPizza);
+    var price = myPizza.calculatePrice();
+    console.log(price);
+
+
+  });
 
 
 
 
-    
-  })
 
-
-
-
-
-})
+});
